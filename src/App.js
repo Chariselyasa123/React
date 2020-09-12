cdc7f2dac6b39affa5e97b4c3f7c1ae9df0de3ab
Chariselyasa123 <ahmad.charis@rharja.info>
2020-09-12 07:57:58 +0700

    memperdalam DOM pada react setState()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Greet} from './components/Greet'
-import Welcome from './components/Welcome'
-import {HelloJSX} from './components/Hallo'
-import {HelloGaPakeJSX} from './components/Hallo'
+// import {Greet} from './components/Greet'
+// import Welcome from './components/Welcome'
+// import {HelloJSX} from './components/Hallo'
+// import {HelloGaPakeJSX} from './components/Hallo'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <Message />
+      <Counter />
       {/* <Greet name="Charis" heroName="Superman"><p>Ini bawahan prop</p></Greet>
       <Greet name="Elyasa" heroName="Batman"> <button>Submit</button></Greet>
       <Greet name="Ahmad" heroName="Wondrman"/>
