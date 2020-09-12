995e09909531ff22d3066528e91b9a103285d448
Chariselyasa123 <ahmad.charis@rharja.info>
2020-09-12 13:14:29 +0700

    belajar function dan class oop event handler

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet'
-import Welcome from './components/Welcome'
+// import Welcome from './components/Welcome'
 // import {HelloJSX} from './components/Hallo'
 // import {HelloGaPakeJSX} from './components/Hallo'
 import Message from './components/Message'
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 function App() {
   return (
     <div className="App">
       <Message />
-      <Counter />
+      {/* <Counter /> */}
       <Greet name="Charis" heroName="Superman" />
       {/* <Greet name="Elyasa" heroName="Batman" /> */}
       {/* <Greet name="Ahmad" heroName="Wondrman"/> */}
-      <Welcome name="Charis" heroName="Superman"/>
+      {/* <Welcome name="Charis" heroName="Superman"/> */}
       {/* <Welcome name="Elyasa" heroName="Batman"/> */}
       {/* <Welcome name="Ahmad" heroName="Wondrman"/> */}
       {/* <HelloJSX />
       <HelloGaPakeJSX /> */}
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
---
+++ src/components/ClassClick.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+
+    clickHandler(){
+        console.log('Ahhh aku di pencet');
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="heder">Pencet tombol dibawah dong :)</h1>
+                <button onClick={this.clickHandler}>Click Aku DOngsz</button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick
