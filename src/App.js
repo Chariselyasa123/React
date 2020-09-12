776d3dd522894c4a210a3e43513b57572eeac204
Chariselyasa123 <ahmad.charis@rharja.info>
2020-09-12 18:59:22 +0700

    beberapa cara melakukan cs react component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet'
+import Inline from './components/Inline';
 // import List from './components/List';
 // import Welcome from './components/Welcome'
 // import {HelloJSX} from './components/Hallo'
 // import {HelloGaPakeJSX} from './components/Hallo'
 import Message from './components/Message'
+import Stylesheet from './components/Stylesheet';
 // import ParrentComponent from './components/ParrentComponent';
 // import UserGreetings from './components/UserGreetings';
 // import Counter from './components/Counter'
 // import FunctionClick from './components/FunctionClick'
 // import ClassClick from './components/ClassClick'
 // import EventBind from './components/EventBind'
+import './appStyle.css'
+import style from './appStyle.module.css'
 
 function App() {
   return (
@@ -33,6 +37,10 @@ function App() {
       {/* <ParrentComponent /> */}
       {/* <UserGreetings /> */}
       {/* <List /> */}
+      <Stylesheet primary={true}/>
+      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={style.success}>Success</h1>
     </div>
   );
 }
